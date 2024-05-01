06bc31253612718d216a08661f738d7326a12065
alialaedin <alialaedin1234@gmail.com>
2024-05-01 11:49:28 +0430

    modify the CartStoreRequest

--- modules/Cart/app/Http/Requests/CartStoreRequest.php
+++ modules/Cart/app/Http/Requests/CartStoreRequest.php
@@ -3,6 +3,7 @@
 namespace Modules\Cart\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Modules\Cart\Models\Cart;
 use Modules\Core\App\Helpers\Helpers;
 use Modules\Product\Models\Product;
 
@@ -21,13 +22,19 @@ class CartStoreRequest extends FormRequest
 		$productId = $this->product_id;
 		$quantity = $this->quantity;
 
-		$product = Product::findOrFail($productId);
+		$product = Product::findOrFail($productId)->with('store:id,balance');
 		$productBalance = $product->store->balance;
 
 		if ($productBalance < $quantity) {
-			throw Helpers::makeValidationException("تعداد انتخابی باید کمتر یا مساوی {$productBalance} باشد!");
+			throw Helpers::makeValidationException("تعداد انتخابی محصول باید کمتر یا مساوی {$productBalance} باشد!");
 		}
-		
+
+		$carts = Cart::whereIn('customer_id', auth()->user()->id)->get();
+		$carts->each(function ($cart) {
+			if ($cart->product_id == $this->product_id) {
+				throw Helpers::makeValidationException("این محصول در سبد خرید موجود است, در صورت نیاز ان را ویرایش کنید");
+			}
+		});
 	}
 
 	public function authorize(): bool
